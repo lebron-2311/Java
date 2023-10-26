20d1f38500dfef98a1c02b7ab3b65e30a3baf71e
lebron-2311 <139559344+lebron-2311@users.noreply.github.com>
2023-10-26 12:00:49 +0800

    Add files via upload

---
+++ test20231026.java
@@ -0,0 +1,17 @@
+public class test20231026 {
+
+    public static void main(String[] args) {
+        short a = 128;
+
+        byte b = (byte) a;
+        System.out.println(a);
+        System.out.println(b);
+        String str1 = "s1151784";
+        String str2 = "郑博文";
+        String str3 = str1 + str2;
+        System.out.println(str3);
+        int c=10;
+        System.out.println("c=="+c);
+
+    }
+}
